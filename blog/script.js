2006595525372adec55dcea076817da658bf43d3
Mercury Rory <Mercury Rory>
2017-01-15 21:42:55 +0300

    I'm plaing with the script. It's awesome.

--- blog/script.js
+++ blog/script.js
@@ -1,29 +1,54 @@
 //Блог владельца блога
-var blog = {
-  notes : [],
-  blocksOfArticles : [],
+var articles = document.querySelector('#articles'),
+    blog = {
+      notes : [],
+      
+	  creatArticle : function( note ) {
+	    var article = document.createElement( 'div' ); 
+	      article.classList.add( 'article' );
+	      article.innerHTML = "<h3 class='article__date'>" +  
+	                          note.date + "</h3><p class='article__paragraph'>" + 
+	                          note.body + "</p>";
+	    articles.appendChild( article ); // Выдает ошибку
+	  },
 
-  generateBlog : function() {
-    blog.notes.map( function( note ) {
-      var article = "<div class='article'><h3 class='article__date'>" +  
-                     note.date + "</h3><p class='article__paragraph'>" + 
-                     note.body + "</p></div>"; 
+	  cleanArticles : function() {
+	    while ( articles.firstChild ) {
+	  	  articles.removeChild( articles.firstChild );
+	  	}
+	  },
 
-      return blog.blocksOfArticles.push(article);
-    });
+	  generateBlog : function() {
+	    blog.notes.map( function( note ) {  
+	      return blog.creatArticle( note );
+	    });
+	  },
+	  searchNote : function() {
+	  	var search = document.querySelector( '#search' ).value.toLowerCase(),
+	  	    noMatches = true,
+	        zeroBlock = document.querySelector('#zero');
 
-    blog.blocksOfArticles.map( function( article ) {
-      return document.querySelector('#articles').innerHTML = article;
-    });
-    // for ( var i = 0; i < blog.notes.length; i++ ) {
-    //    var note = "<div class='article'><h3 class='article__date'>" +  blog.notes[i].date + 
-    //               "</h3><p class='article__paragraph'>" + blog.notes[i].body + "</p></div>"; 
-    //    blog.blocksOfArticles.push(note);            
-    // }
+	    zeroBlock.innerHTML = "";
+	    
+	      blog.cleanArticles();
 
+	    blog.notes.map( function( note ) {
+	 	  var match = note.body
+	 	               .toLowerCase()
+	 	               .indexOf( search );
+		  if ( match != -1 ) {	  
+		  	return blog.creatArticle( note ); 
+		  } 	          
+	 	  
+	 	  return noMatches = false;
+	    });
 
-  }
+	    if ( noMatches != true ) {
+	      return zeroBlock.innerHTML = 'Zero matches.';
+	    }
+	  }
 }
+
 // Конструктор записи
 function Note( date, body ) {
   var newDate = new Date(date);
@@ -33,7 +58,16 @@ function Note( date, body ) {
 
 window.onload = function(e) {
    blog.generateBlog();
+
+   var searchBtn = document.querySelector('#searchBtn'),
+       showNotesBtn = document.querySelector('#showNotes');
+
+   searchBtn.addEventListener('click', blog.searchNote, false);
+   showNotesBtn.addEventListener('click', function() {
+   	 blog.cleanArticles();
+   	 blog.generateBlog();
+   }, false);
 }
-console.log(blog.blocksOfArticles);
+
 var aboutSomething = new Note('01/14/2017', 'Here is text for the article.');
-var anotherOne = new Note('01/15/2017', 'The article on next day.');
+var anotherOne = new Note('01/15/2017', '<i>The article</i> on next day.');
