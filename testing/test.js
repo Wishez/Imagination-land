9a1793b9998246480a02f678e50a76d65b838f9c
Mercury Rory <Mercury Rory>
2017-01-18 15:32:17 +0300

    The games of my mind.

--- testing/test.js
+++ testing/test.js
@@ -1,6 +1,7 @@
 describe( "pow", function() {
   
   describe( "возводит x в степень n", function() {
+
 	function makeTest( x ) {
 	  var expected = x * x * x;
 
@@ -14,7 +15,6 @@ describe( "pow", function() {
       makeTest(i);
     }
 
-
   });
 
   it("при возведении в отрицательную степень результат NaN", function() {
@@ -23,6 +23,24 @@ describe( "pow", function() {
 
   it("при возведении в дробную степень результат NaN", function() {
     assert(isNaN(pow(2, 1.5)));
+  });
+
+  describe("любое число, кроме нуля, встепени 0 равно 1", function() {
+    
+    function makeTest(x) {
+      it("при возведении " + x + " в степень 0 результат: 1", function() {
+      	assert.equal(pow(x, 0), 1);
+      });
+    }
+
+    for ( var i = -5; i <= 5; i += 2 ) {
+      makeTest(i);
+    }
+
+  });
+
+  it("ноль в нулевой степени даёт NaN", function() {
+    assert(isNaN(pow(0, 0)), "0 в степени 0 не NaN");
   });
   
 });
