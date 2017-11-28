68326c4d29c02b513680fc88efc52a1f680b44d2
Filipp Zhuravlev <shiningfinger@list.ru>
2017-11-28 13:30:45 +0300

    Add request pathes to the server.

--- js/work/ContentFilter/noise_supressor/src/constants/conf.js
+++ js/work/ContentFilter/noise_supressor/src/constants/conf.js
@@ -1,1 +1,11 @@
 export const serverUrl = 'https://filipp-zhuravlev.ru';
+
+const getUrl = secondPart => serverUrl + secondPart;
+
+export const loginUrl = getUrl('/account/log_in/');
+export const registerUrl = getUrl('/account/register/');
+export const changePasswordUrl = getUrl('/account/change_password/');
+export const changeEmailUrl = getUrl('/account/change_email/');
+export const recoverPasswordUrl = getUrl('/account/recover_password/');
+export const changeUserAvatarUrl = getUrl('/account/change_user_avatar/');
+export const subscribeUrl = getUrl('/account/subscribe/');
