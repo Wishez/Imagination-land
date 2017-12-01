878256d13fb981201e81eb2b733447f2ffdab18a
Filipp Zhuravlev <shiningfinger@list.ru>
2017-12-01 16:28:48 +0300

    Change the initialization and  add a few props to it.

--- js/work/ContentFilter/js/main.js
+++ js/work/ContentFilter/js/main.js
@@ -2,8 +2,7 @@ import 'jquery';
 import DocumentFilter from './libs/DocumentFilter.js';
 
 const filter = new DocumentFilter({
-	root: document.body
+	root: document.body,
+	backendServerUrl: 'https://filipp-zhuravlev.ru',
+	userWordsUrl: '/words/api/document_data_user/'
 });
-
-
-filter.init(document.domain, window);
