615d90f94c674dad853b45add8d5aabb7360a125
Filipp Zhuravlev <shiningfinger@list.ru>
2018-05-16 18:32:33 +0300

    Remove the content filter.

--- js/work/ContentFilter/js/main.js
+++
@@ -1,8 +0,0 @@
-import 'jquery';
-import DocumentFilter from './libs/DocumentFilter.js';
-
-const filter = new DocumentFilter({
-	root: document.body, //getElementById('page-manager'),
-	backendServerUrl: 'https://filipp-zhuravlev.ru',
-	userWordsUrl: '/words/api/document_data_user/'
-});
